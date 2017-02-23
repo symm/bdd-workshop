9d3fe73a89b6617defe7809996a54b2e27c74232
Gareth Jones <gareth.jones@sainsburys.co.uk>
2017-02-23 10:43:06 +0000

    Add standard feature context and feature

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,64 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use PhpSpec\Exception\Example\PendingException;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * @Given there is a :arg1, which costs £:arg2
+     */
+    public function thereIsAWhichCostsPs($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I add the :arg1 to the basket
+     */
+    public function iAddTheToTheBasket($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should have :arg1 product in the basket
+     */
+    public function iShouldHaveProductInTheBasket($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the overall basket price should be £:arg1
+     */
+    public function theOverallBasketPriceShouldBePs($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should have :arg1 products in the basket
+     */
+    public function iShouldHaveProductsInTheBasket($arg1)
+    {
+        throw new PendingException();
+    }
+}
